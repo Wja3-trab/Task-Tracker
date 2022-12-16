d41fd78aeffba18e1d69253d5289b5c6b30d2955
Ibrahim_Bousba <ibrahim.bousba@gmail.com>
2022-12-16 10:26:40 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,113 @@
-import { checkLength } from "./util.js";
+import { checkLength, appendAllChildren, removeChildren, removeByIndex } from "./util.js";
 window.addEventListener("DOMContentLoaded", () => {
 const taskList = document.querySelector(".tasks");
 const enter = document.querySelector("#inputBtn");
 const textBox = document.getElementById("taskIn");
+const pLvl = document.getElementById("pLvl");
+
+let highArr = [];
+let midArr = [];
+let lowArr = [];
+
+var black = "#010d01";
+var green = "#4ff141";
+var red = "#c95555";
+var yellow = "#eeca19";
+
+function checkStyle() {
+    const style = pLvl.style;
+    style.backgroundColor = black;
+    if(pLvl.selectedIndex === 0) {
+        style.borderColor = red;
+        style.color = red;
+    }
+    if(pLvl.selectedIndex === 1) {
+        style.borderColor = yellow;
+        style.color = yellow;
+    }
+    if(pLvl.selectedIndex === 2) {
+        style.borderColor = green;
+        style.color = green;
+    }
+}
+
+
+checkStyle();
+
+pLvl.addEventListener("click", function() {
+    checkStyle()
+})
+
+pLvl.addEventListener("keyup", function() {
+    checkStyle()
+})
 
 enter.addEventListener("click", function() {
+    let fullArr = [];
     if(checkLength(textBox) === true) {
+
+        taskList.innerHTML = "";
         const li = document.createElement("li");
-        const delBtn = document.createElement("button");
-        const editBtn = document.createElement("button");
-        delBtn.classList.add("delBtn");
-        delBtn.innerText = "Del";
-        editBtn.classList.add("editBtn");
-        editBtn.innerText = "Edit";
+        
         li.innerText = textBox.value;
-        taskList.appendChild(li);
-        li.appendChild(delBtn);
-        li.appendChild(editBtn);
+
+        if(pLvl.value === "High") {
+            li.classList.add("High");
+            highArr.push(li);
+        }
+        if(pLvl.value === "Medium") {
+            li.classList.add("Medium");
+            midArr.push(li);
+        }
+        if(pLvl.value === "Low") {
+            li.classList.add("Low");
+            lowArr.push(li);
+        }
+
+        fullArr = fullArr.concat(highArr, midArr, lowArr);
+        appendAllChildren(taskList, fullArr);
+
+        console.log(fullArr);
+
         textBox.value = "";
     }
 });
 
 taskList.addEventListener("click", function(e) {
-    const parent = e.target.parentElement;
-    if(e.target.tagName === "LI") {
-        e.target.classList.toggle("done");
-    }
-    else if(e.target.className === "delBtn") {
+    const target = e.target;
+    const parent = target.parentElement;
+
+    if(target.tagName === "LI") {
+        target.classList.toggle("done");
+    } 
+    else if(target.className === "delBtn") {
+        if(parent.classList.contains("High")) {
+            removeByIndex(highArr, parent);
+        }
+        else if(parent.classList.contains("Medium")) {
+            removeByIndex(midArr, parent);
+        }
+        else {
+            removeByIndex(lowArr, parent);
+        }
         parent.remove();
-    }
-    else if(e.target.className === "editBtn" && !checkLength(textBox)) {
+    } 
+    else if(target.className === "editBtn" && !checkLength(textBox)) {
+        if(parent.classList.contains("High")) {
+            removeByIndex(highArr, parent);
+            pLvl.value = "High";
+        }
+        else if(parent.classList.contains("Medium")) {
+            removeByIndex(midArr, parent);
+            pLvl.value = "Medium";
+        }
+        else {
+            removeByIndex(lowArr, parent);
+            pLvl.value = "Low";
+        }
         textBox.value = parent.childNodes[0].nodeValue;
+        checkStyle();
         parent.remove();
     }
 }, false);
-
 });
--- util.js
+++ util.js
@@ -1,5 +1,36 @@
-export {checkLength};
+export {checkLength, appendAllChildren, removeChildren, removeByIndex};
 //See if a string is not empty
 function checkLength(str) {
     if(str.value.length > 0) return true;
+}
+
+//Loop through a given array and append them to an element
+function appendAllChildren(elem, someArr) {
+    for(let i = 0; i < someArr.length; i++) {
+        const child1 = document.createElement("button");
+        const child2 = document.createElement("button");
+        child1.classList.add("delBtn");
+        child1.textContent = "Del";
+        child2.classList.add("editBtn");
+        child2.textContent = "Edit";
+        elem.appendChild(someArr[i]);
+        if(someArr[i].childNodes.length === 1) {
+            someArr[i].appendChild(child1);
+            someArr[i].appendChild(child2);
+        }
+    }
+}
+
+//removing all children of a node
+function removeChildren(node) {
+    while(node.firstChild) {
+        node.removeChild(node.lastChild);
+    }
+}
+
+//remove index in an array
+
+function removeByIndex(myArr, val) {
+    const x = myArr.indexOf(val);
+    const y = myArr.splice(x, 1);
 }
